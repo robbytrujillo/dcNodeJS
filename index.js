25f2c9f52980e9fb05880875a309ab7c0a40b8d2
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-06 09:01:05 +0700

    Using Package

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-var http = require('http');
+import variableName from 'package-name';
+import _ from 'lodash';
 
-var port = 8080;
-http.createServer(function (req, res) {
-	console.log("Aplikasi berjalan pada port : "+port);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(port);
+const myArray = [1, 2, 3, 4];
+let sum = 0;
+
+for(let i = 0; i < myArray.length; i++) {
+  sum += myArray[i];
+}
+
+console.log(sum);
+
+/* output
+10
+*/
+
+const myArray1 = [1, 2, 3, 4];
+let sum1 = myArray.reduce((prev, curr) => {
+  return prev + curr;
+});
+
+console.log(sum1);
+
+/* output
+10
+*/
+
+
+const myArray2 = [1, 2, 3, 4];
+const sum2 = _.sum(myArray2);
+
+console.log(sum);
+
+/* output
+10
+*/
+
+// var http = require('http');
+
+// var port = 8080;
+// http.createServer(function (req, res) {
+// 	console.log("Aplikasi berjalan pada port : "+port);
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(port);
